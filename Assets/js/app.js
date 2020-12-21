cbde0924c4c43854c6c5c5093e2b903481b2328c
Marco Jansen <marco.jansen@gmx.us>
2020-12-21 16:10:29 +0800

    added basic js with on click functions and 2 API requests temporary printed to console.log

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,1 +1,48 @@
-// js script comes here
+// document Ready Wrapper MJ
+$(document).ready(function () {
+    
+    // init Function MJ
+    init()
+    function init(){
+       $("#intro").show()
+    }
+
+    // hide Start Button On Click MJ
+    $("#startLOTR").on("click", function(){
+       $("#intro").hide()
+    })
+    
+    // on Click Of Start Button Request LOTR API MJ
+    $("#startButton").on("click", function(){
+ 
+        $.ajax({
+            url: 'https://the-one-api.dev/v2/character',
+            type: 'GET',
+            contentType: 'application/json',
+            headers: {
+            'Authorization': 'Bearer 8dD_KqhUELsw37ZZ8_2t'
+            },
+            success: function (result) {
+                console.log(JSON.stringify(result));             
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    });
+
+    // on Click Of Start Button Request Harry Potter API MJ
+    $("#startButton").on("click", function(){
+
+        $.ajax({
+            url: 'https://hp-api.herokuapp.com/api/characters',
+            method: "GET"
+        }).then(function(response){  
+            console.log(response)
+        });
+
+    });
+});
+    // // functionToRequestResponseFromHarryPotterURL
+    
+    
