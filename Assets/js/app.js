cb7dcc7644db6ff3042afd7074dd4057b80ecbbd
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2020-12-23 14:48:41 +0800

    adding draft timer code

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,1 +1,21 @@
 // js script comes here
+
+// Create Element to be grabbed for Timer Display
+const timerDisplay = document.querySelector('.timer');
+
+// Start Timer on Click Code
+const startTimer = function () {
+    // When Btn is clicked begin timer at 30 seconds (can be modified)
+    let timeRemaining = 30;
+    // Call the timer every second
+    setInterval(() => {
+        // with each call print the time remaining to the User Interface
+        timerDisplay.textContent = timeRemaining;     
+        // Decrease timer by 1 second for each call 
+        time--;
+        // When Timer reaches zero end game, show user score and high score log
+    }, 1000);
+}
+
+// Call funtion to start timer on click of start button - add to eventlistener
+startTimer();
