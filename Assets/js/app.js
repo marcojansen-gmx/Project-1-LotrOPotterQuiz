db4344705b8524588648d4bc4f8e667a9e2663a3
Marco Jansen <marco.jansen@gmx.us>
2020-12-31 15:22:22 +0800

    added required HTML for quizcontainer, added random API selection and basic quiz layout

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,62 +1,127 @@
-// document Ready Wrapper MJ
-$(document).ready(function () {
+$('document').ready(function(){
+
+// Create Element to be grabbed for Timer Display
+const timerDisplay = document.querySelector(".timer");
+let timer;
+let HPCharacters;
+let LOTRCharacters;
+let choosenAPI;
+let randomArrayNo;
     
     // init Function MJ
     init()
     function init(){
-       $("#startLOTR").show()
+       $("#intro").show()
+       $(".timer").hide()
+       $("#quizContainer").hide()
+
     }
 
-    // hide Start Button On Click MJ
-    $("#startLOTR").on("click", function(){
-       $("#startLOTR").hide()
-    })
-    
-    // on Click Of Start Button Request LOTR API MJ
     $("#startButton").on("click", function(){
- 
-        $.ajax({
-            url: 'https://the-one-api.dev/v2/character',
-            type: 'GET',
-            contentType: 'application/json',
-            headers: {
-            'Authorization': 'Bearer 8dD_KqhUELsw37ZZ8_2t'
-            },
-            success: function (result) {
-                const resultLOTRAPI = (JSON.stringify(result))
-                console.log(resultLOTRAPI);
-                return resultLOTRAPI;
-            },
-            error: function (error) {
-                console.log(error);
-            }
-        });
-    });
-
-    // on Click Of Start Button Request Harry Potter API MJ
-    $("#startButton").on("click", function(){
-
-        $.ajax({
+      $("#intro").hide()    
+      $.ajax({
+        url: 'https://the-one-api.dev/v2/character',
+        method: "GET",
+        contentType: 'application/json',
+        headers: {
+        'Authorization': 'Bearer 8dD_KqhUELsw37ZZ8_2t'
+        }
+    }).then(function(response){ 
+        let resultLOTRAPI = (JSON.stringify(response));
+        LOTRCharacters = response.docs;
+        //   console.log(resultLOTRAPI);  
+          console.log("LOTRArray:" + Array.isArray(LOTRCharacters));   
+        // second ajax call for Harry Potter API
+          $.ajax({
             url: 'https://hp-api.herokuapp.com/api/characters',
             method: "GET"
-        }).then(function(response){  
-            const resultHarryAPI = (JSON.stringify(response));
-            console.log(resultHarryAPI);
-            return resultHarryAPI;
-        });
-
+        }).then(function(response){ 
+                let resultHarryAPI = (JSON.stringify(response));
+                HPCharacters = response;
+                // console.log(resultHarryAPI);  
+                console.log("HarryArray:" + Array.isArray(HPCharacters));
+                // quiz timer start
+                if (timer) clearInterval(timer);
+                $(".timer").show()
+                timer = startTimer();
+                // quiz start
+                $("#quizContainer").show()
+                startQuiz(LOTRCharacters,HPCharacters);       
+        }); 
     });
+    
 
-    // random API Selector MJ
-    function selectRandomAPIResult(){
+        // $.ajax({
+        //     url: 'https://the-one-api.dev/v2/character',
+        //     type: 'GET',
+        //     contentType: 'application/json',
+        //     headers: {
+        //     'Authorization': 'Bearer 8dD_KqhUELsw37ZZ8_2t'
+        //     },
+        //     success: function (result) {
+        //         const resultLOTRAPI = (JSON.stringify(result));
+        //         // console.log(resultLOTRAPI);
+        //         LOTRCharacters = result.docs;
+        //         console.log("LOTRArray:" + Array.isArray(LOTRCharacters));
+        //     },
+        //     error: function (error) {
+        //         console.log(error);
+        //     }
+        // });
+
+        // random API Selector MJ
+        function selectRandomAPIResult(){
         let selectorAPI = Math.round(Math.random());
-        console.log(selectorAPI);
         let arrayAPIChoice = ["resultLOTRAPI","resultHarryAPI"];
-        let choosenAPI = arrayAPIChoice[selectorAPI];
+        choosenAPI = arrayAPIChoice[selectorAPI];
         console.log(choosenAPI);
-        return(choosenAPI);
     };
+
+        function startQuiz(LOTRCharacters,HPCharacters){
+        selectRandomAPIResult();
+        if (choosenAPI === "resultLOTRAPI") {
+            console.log(LOTRCharacters);
+            randomArrayNo = Math.floor(Math.random() * LOTRCharacters.length);
+            console.log(randomArrayNo, LOTRCharacters[randomArrayNo].name);
+            $("#question").text(`From which book is this char? Name: ${LOTRCharacters[randomArrayNo].name}`);
+            $("#quizContainer").attr("data-answer", "buttonLOTR");
+    }
+        else{
+            console.log(HPCharacters);
+            randomHPAPIChar = Math.floor(Math.random() * HPCharacters.length);
+            console.log(randomHPAPIChar, HPCharacters[randomHPAPIChar]);
+            $("#question").text(`From which book is this char? Name: ${HPCharacters[randomHPAPIChar].name}`);
+            $("#quizContainer").attr("data-answer", "buttonHP");
+    }
+
+    }
 
 });
    
-    
+// Start Timer on Click Code
+const startTimer = function () {
+    const tick = function() {
+
+        const min = String(Math.trunc(time / 60)).padStart(2, 0);
+        const sec = String(time % 60).padStart(2, 0);
+        // with each call print the time remaining to the User Interface
+        timerDisplay.textContent = `${min}:${sec}`;     
+        // When Timer reaches zero end game, show user score and high score log
+        if (time === 0) {
+            clearInterval(timer);
+            console.log("Game Over");
+            console.log("Your Score will be displayed");
+        }
+
+        // Decrease timer by 1 second for each call 
+        time--;
+    }
+    // When Btn is clicked begin timer at 30 seconds (can be modified)
+    let time = 5;
+    // Call the timer every second
+    tick();
+    const timer = setInterval(tick, 1000);
+    return timer;
+}
+
+});
