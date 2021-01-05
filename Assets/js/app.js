3f5cd3eb989c903a305fba5c6f2ff2a807966490
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-01-05 17:30:33 +0800

    update using MJ clean code

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,54 +1,63 @@
-$('document').ready(function(){
-    // Create Element to be grabbed for Timer Display
-    const timerDisplay = document.querySelector(".timer");
-    let timer;
-    let HPCharacters;
-    let LOTRCharacters;
-    let choosenAPI;
-    let randomArrayNo;
-    let currentScore;
-    let currentMultiplier;
-    let isAnswerHP;
-    let isAnswerLOTR;
-        // init Function MJ
-        init()
-        function init(){
-           $("#intro").show()
-           $(".timer").hide()
-           $("#quizContainer").hide()
-        }
-        $("#startButton").on("click", function(){
-          $("#intro").hide()    
-          $.ajax({
-            url: 'https://the-one-api.dev/v2/character',
-            method: "GET",
-            contentType: 'application/json',
-            headers: {
-            'Authorization': 'Bearer 8dD_KqhUELsw37ZZ8_2t'
+
+    $('document').ready(function(){
+
+        // Create Element to be grabbed for Timer Display
+        const timerDisplay = document.querySelector(".timer");
+        let timer;
+        let HPCharacters;
+        let LOTRCharacters;
+        let choosenAPI;
+        let randomArrayNo;
+        let currentScore;
+        let currentMultiplier;
+        let isAnswerHP;
+        let isAnswerLOTR;
+
+            // init Function MJ
+            init()
+            function init(){
+                $("#intro").show()
+                $(".timer").hide()
+                $("#quizContainer").hide()
+        };
+
+            // on click of start button run API requests
+            $("#startButton").on("click", function(){
+                $("#intro").hide()    
+                $.ajax({
+                url: 'https://the-one-api.dev/v2/character',
+                method: "GET",
+                contentType: 'application/json',
+                headers: {
+                'Authorization': 'Bearer 8dD_KqhUELsw37ZZ8_2t'
             }
-        }).then(function(response){ 
-            let resultLOTRAPI = (JSON.stringify(response));
-            LOTRCharacters = response.docs;
-            //   console.log(resultLOTRAPI);  
-              console.log("LOTRArray:" + Array.isArray(LOTRCharacters));   
-            // second ajax call for Harry Potter API
-              $.ajax({
+            // handle LOTR API result
+            }).then(function(response){ 
+                let resultLOTRAPI = (JSON.stringify(response));
+                LOTRCharacters = response.docs;
+                // console.log(resultLOTRAPI);  
+                // console.log("LOTRArray:" + Array.isArray(LOTRCharacters));   
+                $.ajax({
                 url: 'https://hp-api.herokuapp.com/api/characters',
                 method: "GET"
-            }).then(function(response){ 
+                // handle HP API result
+                }).then(function(response){ 
                     let resultHarryAPI = (JSON.stringify(response));
                     HPCharacters = response;
                     // console.log(resultHarryAPI);  
-                    console.log("HarryArray:" + Array.isArray(HPCharacters));
+                    // console.log("HarryArray:" + Array.isArray(HPCharacters));
                     // quiz timer start
                     if (timer) clearInterval(timer);
                     $(".timer").show()
                     timer = startTimer();
                     // quiz start
                     $("#quizContainer").show()
-                    startQuiz(LOTRCharacters,HPCharacters);       
-            }); 
+                    startQuiz(LOTRCharacters,HPCharacters);              
+                }); 
+
+            });
         });
+
             // random API Selector MJ
             function selectRandomAPIResult(){
             let selectorAPI = Math.round(Math.random());
@@ -56,103 +65,136 @@ $('document').ready(function(){
             choosenAPI = arrayAPIChoice[selectorAPI];
             console.log(choosenAPI);
         };
+
+            // function to startquiz
             function startQuiz(LOTRCharacters,HPCharacters){
             selectRandomAPIResult();
+                // LOTR random selection
                 if (choosenAPI === "resultLOTRAPI") {
-                    console.log(LOTRCharacters);
+                    // console.log(LOTRCharacters);
                     randomArrayNo = Math.floor(Math.random() * LOTRCharacters.length);
-                    console.log(randomArrayNo, LOTRCharacters[randomArrayNo].name);
+                    // console.log(randomArrayNo, LOTRCharacters[randomArrayNo].name);
                     $("#question").text(`From which book is this char? Name: ${LOTRCharacters[randomArrayNo].name}`);
                     $("#quizContainer").attr("data-answer", "buttonLOTR");
             }
-                else{
-                    console.log(HPCharacters);
-                    randomHPAPIChar = Math.floor(Math.random() * HPCharacters.length);
-                    console.log(randomHPAPIChar, HPCharacters[randomHPAPIChar]);
-                    $("#question").text(`From which book is this char? Name: ${HPCharacters[randomHPAPIChar].name}`);
-                    $("#quizContainer").attr("data-answer", "buttonHP");
-        }
-    }
-    });
-    // LOTR button
-    $("#buttonHP").on("click", function(){
-        let answerAttribute=$("#quizContainer").attr("data-answer");
-        console.log(answerAttribute);
-        isAnswerLOTR=("buttonHP"===answerAttribute);
-        if (isAnswerLOTR){
-            console.log("correct");
-        }
-        else{console.log("incorrect");
-        }
-    });
-    $("#buttonLOTR").on("click", function(){
-        let answerAttribute=$("#quizContainer").attr("data-answer");
-        console.log(answerAttribute);
-        isAnswerLOTR=("buttonLOTR"===answerAttribute);
-        if (isAnswerLOTR){
-            console.log("correct");
-        }
-        else{console.log("incorrect");
-        }
-    });
+                    else{
+                        // console.log(HPCharacters);
+                        randomHPAPIChar = Math.floor(Math.random() * HPCharacters.length);
+                        console.log(randomHPAPIChar, HPCharacters[randomHPAPIChar]);
+                        $("#question").text(`From which book is this char? Name: ${HPCharacters[randomHPAPIChar].name}`);
+                        $("#quizContainer").attr("data-answer", "buttonHP");
+                }
+        };
 
-    // Calculate score
-    function scoreCalc(isAnswerEval){
-        currentScore = 0;
-        currentMultiplier=2;
-        if (isAnswerEval) {
-            if (currentScore==undefined) {
-                currentMultiplier=2;
-                currentScore=1;
-                console.log("A:"+currentMultiplier);
-                console.log("B:"+currentScore);
-            } else {
-                currentMultiplier=(currentMultiplier+1);
-                currentScore=(currentScore*currentMultiplier);
-                console.log("C:"+currentMultiplier);
-                console.log("D:"+currentScore);
-            }
+            // HP button evaluation on click
+            $("#buttonHP").on("click", function(){
+                let answerAttribute=$("#quizContainer").attr("data-answer");
+                console.log(answerAttribute);
+                isAnswerEval=("buttonHP"===answerAttribute);
+                    if (isAnswerEval){
+                        console.log("correct");
+                        scoreCalc(isAnswerEval);
+                }
+                    else{
+                        console.log("incorrect");
+                        scoreCalc(isAnswerEval);
+                }
+
+        });
+
+            // LOTR button evaluation on on click
+            $("#buttonLOTR").on("click", function(){
+                let answerAttribute=$("#quizContainer").attr("data-answer");
+                console.log(answerAttribute);
+                isAnswerEval=("buttonLOTR"===answerAttribute);
+                    if (isAnswerEval){
+                        console.log("correct");
+                        scoreCalc(isAnswerEval);
+                }
+                    else{
+                        console.log("incorrect");
+                        scoreCalc(isAnswerEval);
+                }
+        });
+   
+            // LOTR button evaluation
+            $("#buttonLOTR").on("click", function(){
+                let answerAttribute=$("#quizContainer").attr("data-answer");
+                console.log(answerAttribute);
+                isAnswerLOTR=("buttonLOTR"===answerAttribute);
+                    if (isAnswerLOTR){
+                        console.log("correct");
+                }
+                    else{
+                        console.log("incorrect");
+                        scoreCalc(isAnswerEval);
+                }
+        });
+    
+        // Calculate score
+        function scoreCalc(isAnswerEval){
+            // set initial variable values
+            currentScore;
+            currentMultiplier=2;
+            // with score still empty set initial current score and multiplier
+            if (isAnswerEval) {
+                if (currentScore==undefined) {
+                    currentMultiplier=2;
+                    currentScore=1;
+                    console.log("A:"+currentMultiplier);
+                    console.log("B:"+currentScore);
+                }   
+                    // with score already set add +1 to multiplier and mutliply with current score
+                    else {
+                    currentMultiplier=(currentMultiplier+1);
+                    currentScore=(currentScore*currentMultiplier);
+                    console.log("C:"+currentMultiplier);
+                    console.log("D:"+currentScore);
+                }
         }
-        else {
-            //added if statement for incorrect first answer under a false
-            if (currentScore==undefined) {
-                currentMultiplier=2;
-                currentScore=0;
-                console.log("E:"+currentMultiplier);
-                console.log("F:"+currentScore);
-            } else {
-                currentMultiplier=2;
-                currentScore=(currentScore*currentMultiplier);
-                console.log("G:"+currentMultiplier);
-                console.log("H:"+currentScore);
-            }
-            return currentScore
-        }   
+                else {
+                //added if statement for incorrect first answer under a evalualtion that is false
+                    if (currentScore==undefined) {
+                        currentMultiplier=2;
+                        currentScore=0;
+                        console.log("E:"+currentMultiplier);
+                        console.log("F:"+currentScore);
+                }               else {
+                            currentMultiplier=2;
+                            currentScore=(currentScore*currentMultiplier);
+                            console.log("G:"+currentMultiplier);
+                            console.log("H:"+currentScore);
+                    }
+            }   
     };
-    // Start Timer on Click Code
-    const startTimer = function () {
-        const tick = function() {
-            const min = String(Math.trunc(time / 60)).padStart(2, 0);
-            const sec = String(time % 60).padStart(2, 0);
-            // with each call print the time remaining to the User Interface
-            timerDisplay.textContent = `${min}:${sec}`;     
-            // When Timer reaches zero end game, show user score and high score log
-            if (time === 0) {
-                clearInterval(timer);
-                console.log("Game Over");
-                console.log("Your Score will be displayed" );
-                renderEndGame();
+
+            // Start Timer on Click Code
+            const startTimer = function () {
+                const tick = function() {
+                const min = String(Math.trunc(time / 60)).padStart(2, 0);
+                const sec = String(time % 60).padStart(2, 0);
+                // with each call print the time remaining to the User Interface
+                timerDisplay.textContent = `${min}:${sec}`;     
+                // When Timer reaches zero end game, show user score and high score log
+                    if (time === 0) {
+                        clearInterval(timer);
+                        console.log("Game Over");
+                        console.log("Your Score will be displayed");
+                        renderEndGame();
+                }
+            
+                // Decrease timer by 1 second for each call 
+                time--;
             }
-            // Decrease timer by 1 second for each call 
-            time--;
-        }
-        // When Btn is clicked begin timer at 30 seconds (can be modified)
-        let time = 5;
-        // Call the timer every second
-        tick();
-        const timer = setInterval(tick, 1000);
-        return timer;
-    }
+    
+            // When Btn is clicked begin timer at 30 seconds (can be modified)
+            let time = 5;
+            // Call the timer every second
+            tick();
+            const timer = setInterval(tick, 1000);
+            return timer;
+    };
+            
 });
 
 // creating container element to add end of game elements
@@ -175,10 +217,10 @@ $('document').ready(function(){
         containerElement.innerHTML = "";
         // Game is Over Notification
         const endGameMessageElement = document.createElement('div');
-        endGameMessageElement.setAttribute('class', 'myClassRed');
+        // endGameMessageElement.setAttribute('class', 'h4');
         endGameMessageElement.innerText = "Game is Over!";
         // Let user know what their score is
-        const userScoreMessageElement = document.createElement('h4');
+        const userScoreMessageElement = document.createElement('div');
         userScoreMessageElement.innerHTML = "Your final score is: ";
         endGameMessageElement.append(userScoreMessageElement);
         // Request user input 
